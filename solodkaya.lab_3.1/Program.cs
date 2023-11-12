ecbf87a1278255e8e0cd1f8b6dc515a0bf553adb
Arina <Arina@DESKTOP-H3T0JAQ>
2023-11-12 15:10:11 +0200

    Добавьте файлы проекта.

---
+++ solodkaya.lab_3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿try
+{
+    Console.Write("Введите k:");
+    int k = int.Parse(Console.ReadLine());
+    double W = 1;
+
+    for (int i = 1; i <= k; i++)
+    {
+        double fact = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            fact *= j;
+        }
+
+        W += Math.Pow(-1, i) * Math.Pow(i - 3, 2) / fact;
+    }
+
+    Console.WriteLine($"W={W:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
